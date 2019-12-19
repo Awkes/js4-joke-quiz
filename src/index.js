3fa16f1964f981e922a88733fb97197bc02f4dc9
Andreas Åkerlöf <andreas@akerlof.se>
2019-12-19 13:03:09 +0100

    added global styles and fonts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+require('typeface-bowlby-one-sc');
+require('typeface-inconsolata');
+require('typeface-acme');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
