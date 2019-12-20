11f15cd1b36e52eb0fe96a49468e66811e52b3ff
Awkes <andreas@akerlof.se>
2019-12-20 16:10:47 +0100

    fixed wrong naming of space

--- src/theme.js
+++ src/theme.js
@@ -6,7 +6,7 @@ export default {
   },
   fontSizes: [16, 24, 32, 40, 56, 72],
   letterSpacing: [0, 1, 2, 3],
-  spaces: [0, 5, 10, 15, 20, 30, 40, 50, 75, 100, 200],
+  space: [0, 5, 10, 15, 20, 30, 40, 50, 75, 100, 200],
   colors: {
     text: '#000',
     background: '#eee',
